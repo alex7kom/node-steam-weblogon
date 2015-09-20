6f09061f8af8576017cef6889b6899e9bd16d0e8
Alexey Komarov <alex7kom@gmail.com>
2015-09-20 16:55:26 +0300

    Improved code style

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var SteamCrypto = require('steam-crypto');
 
 var SteamEResultOK = 1;
 
-function handleLogOnResponse(logOnResponse) {
-  if (logOnResponse.eresult == SteamEResultOK) {
+function handleLogOnResponse (logOnResponse) {
+  if (logOnResponse.eresult === SteamEResultOK) {
     this._webLoginKey = logOnResponse.webapi_authenticate_user_nonce;
   }
 }
 
-function SteamWebLogOn(steamClient, steamUser) {
+function SteamWebLogOn (steamClient, steamUser) {
   this._steamClient = steamClient;
   this._steamUser = steamUser;
 
@@ -22,11 +22,14 @@ SteamWebLogOn.prototype.webLogOn = function (callback) {
   getInterface('ISteamUserAuth').post('AuthenticateUser', 1, {
     steamid: this._steamClient.steamID,
     sessionkey: sessionKey.encrypted,
-    encrypted_loginkey: SteamCrypto.symmetricEncrypt(new Buffer(this._webLoginKey), sessionKey.plain)
-  }, function(statusCode, body) {
-    if (statusCode != 200) {
+    encrypted_loginkey: SteamCrypto.symmetricEncrypt(
+      new Buffer(this._webLoginKey),
+      sessionKey.plain
+    )
+  }, function (statusCode, body) {
+    if (statusCode !== 200) {
       // request a new login key first
-      this._steamUser.requestWebAPIAuthenticateUserNonce(function(nonce) {
+      this._steamUser.requestWebAPIAuthenticateUserNonce(function (nonce) {
         this._webLoginKey = nonce.webapi_authenticate_user_nonce;
         this.webLogOn(callback);
       }.bind(this));
